a7178e253430cbce42edd579e12d7b07c65550cc
johannesloibl <johannesloibl@users.noreply.github.com>
2024-07-17 10:46:22 +0000

    deploy: 37f3d8f43915e410a926003d0d6e1aea582a186c

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.7.3.dev5+g37f3d8f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
